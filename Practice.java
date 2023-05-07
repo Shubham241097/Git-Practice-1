f2829829ba388239e09e55c5a292abe84e6269f2
Shubham Khilari <shubhamkhilari@yahoo.com>
2023-05-07 17:06:27 +0530

    BubbleSort

--- Practice.java
+++ Practice.java
@@ -12,9 +12,22 @@ public class Sorting{
         }
     }
 
+    public static void bubbleSort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1;j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
     public static void main(String[]args){
 
         int arr[] = {3,8,2,6,4,5};
         selectionSort(arr);
+        bubbleSort(arr);
     }
 }
